9eb3e3d895fc8e6986a77e702c1ee35c02f7a000
Rafal240 <73700997+Rafal240@users.noreply.github.com>
2021-05-16 12:07:08 +0200

    Dodany komponent Section

---
+++ src/Components/Section.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Section = ({className,children}) => {
+    return (
+        <section className={className}>
+        </section>
+    )
+}
+export default Section
