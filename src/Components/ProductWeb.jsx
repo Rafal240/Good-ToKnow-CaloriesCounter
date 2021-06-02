58c47018cde0f5f3588e4d97d76b4d9a73052729
Rafal240 <73700997+Rafal240@users.noreply.github.com>
2021-06-02 18:07:36 +0200

    form

--- src/Components/ProductWeb.jsx
+++ src/Components/ProductWeb.jsx
@@ -38,41 +38,47 @@ const ProductWeb = () => {
   };
 
   return (
-    <Section className="add__contrainer ">
-      <Div className="add__wrapper">
-        <Div className="add__title">
-          <h2 className="title__text">Add your food</h2>
-        </Div>
+    <form>
+      <Section className="add__contrainer ">
+        <Div className="add__wrapper">
+          <Div className="add__title">
+            <h2 className="title__text">Add your food</h2>
+          </Div>
 
-        <Div className="add__form">
-          <Div className="food__name">
-            <Paragraf className="form__text" text="Enter product name:" />
-            <InputText value={name} onChange={onNameChangeHandler} />
+          <Div className="add__form">
+            <Div className="food__name">
+              <Paragraf className="form__text" text="Enter product name:" />
+              <InputText value={name} onChange={onNameChangeHandler} />
+            </Div>
+            <Div className="food__calories">
+              <Paragraf
+                className="form__text"
+                text="Product Calories"
+              ></Paragraf>
+              <InputText value={calories} onChange={onCaloriesChangeHandler} />
+            </Div>
+            <Div className="food__fat">
+              <Paragraf className="form__text" text="Fat"></Paragraf>
+              <InputText value={fat} onChange-={onFatChangeHandler} />
+            </Div>
+            <Div className="food__carbs">
+              <Paragraf className="form__text" text="Carbs"></Paragraf>
+              <InputText value={carb} onChange-={onCarbChangeHandler} />
+            </Div>
+            <Div className="food__proteins">
+              <Paragraf className="form__text" text="Proteins"></Paragraf>
+              <InputText value={protein} onChange={onProteinChangeHandler} />
+            </Div>
+            <Div className="food__sugar">
+              <Paragraf className="form__text" text="Sugar"></Paragraf>
+              <InputText value={sugar} onChange={onSugarChangeHandler} />
+            </Div>
+
+            <ButtonProduct onClick={onAddProductHandler} />
           </Div>
-          <Div className="food__calories">
-            <Paragraf className="form__text" text="Product Calories"></Paragraf>
-            <InputText value={calories} onChange={onCaloriesChangeHandler} />
-          </Div>
-          <Div className="food__fat">
-            <Paragraf className="form__text" text="Fat"></Paragraf>
-            <InputText value={fat} onChange-={onFatChangeHandler} />
-          </Div>
-          <Div className="food__carbs">
-            <Paragraf className="form__text" text="Carbs"></Paragraf>
-            <InputText value={carb} onChange-={onCarbChangeHandler} />
-          </Div>
-          <Div className="food__proteins">
-            <Paragraf className="form__text" text="Proteins"></Paragraf>
-            <InputText value={protein} onChange={onProteinChangeHandler} />
-          </Div>
-          <Div className="food__sugar">
-            <Paragraf className="form__text" text="Sugar"></Paragraf>
-            <InputText value={sugar} onChange={onSugarChangeHandler} />
-          </Div>
-          <ButtonProduct onClick={onAddProductHandler} />
         </Div>
-      </Div>
-    </Section>
+      </Section>
+    </form>
   );
 };
 export default ProductWeb;
