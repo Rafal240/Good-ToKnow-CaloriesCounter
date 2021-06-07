c0be17c4428e346ef0b68cef8d67145f79aa2a45
Rafal240 <73700997+Rafal240@users.noreply.github.com>
2021-06-07 13:06:30 +0200

    lunch component

--- src/Components/Lunch.jsx
+++ src/Components/Lunch.jsx
@@ -9,38 +9,93 @@ import LunchExistingItems from "./MainContainer/LunchExistingItems";
 
 const Lunch = () => {
   const [data, setData] = useState([]);
-  const items = [
-    {
-      name: "Soup",
-      cal: 122,
-      fat: "12g",
-      carb: "3.15g",
-      protein: "5.02",
-      sugar: "5.5g",
-    },
-    {
-      name: "Sandwich",
-      cal: 100,
-      fat: "15g",
-      carb: "7.05g",
-      protein: "4.22",
-      sugar: "10.5g",
-    },
-  ];
+  const [grams, setGrams] = useState("");
+  const [select, setSelect] = useState("");
+  const onChangeGrams = (event) => setGrams(event.target.value);
+  const handleSelectChange = (event) => {
+    setSelect(event.target.value);
+  };
+  const [items, setItems] = useState([]);
+  const AddItemButton = () => {
+    fetch("http://localhost:3000/lunch", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: select,
+      }),
+    }).then(() => {
+      setSelect("");
+      getData();
+    });
+  };
+  const onAddProductHandler = () => {
+    fetch("http://localhost:3000/all_products", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+  // const items = [
+  //   {
+  //     name: "Soup",
+  //     cal: 122,
+  //     fat: "12g",
+  //     carb: "3.15g",
+  //     protein: "5.02",
+  //     sugar: "5.5g",
+  //   },
+  //   {
+  //     name: "Sandwich",
+  //     cal: 100,
+  //     fat: "15g",
+  //     carb: "7.05g",
+  //     protein: "4.22",
+  //     sugar: "10.5g",
+  //   },
+  // ];
+  const getData = () => {
+    fetch("http://localhost:3000/all_products")
+      .then((response) => response.json())
+      .then((all_products) => {
+        setData(all_products);
+        return fetch("http://localhost:3000/lunch")
+          .then((response) => response.json())
+          .then((breakfastItem) => {
+            const items = breakfastItem.map((eachBreakfastItem) => {
+              const productDetails = all_products.find(
+                (product) => product.name === eachBreakfastItem.name
+              );
+              return productDetails;
+            });
+            setItems(items);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
 
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   useEffect(() => {
-    const getData = () => {
-      fetch("http://localhost:3000/all_products")
-        .then((response) => response.json())
-        .then((data) => {
-          setData(data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    };
     getData();
   }, []);
+  // useEffect(() => {
+  //   const getData = () => {
+  //     fetch("http://localhost:3000/all_products")
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         setData(data);
+  //       })
+  //       .catch((error) => {
+  //         console.log(error);
+  //       });
+  //   };
+  //   getData();
+  // }, []);
   return (
     <Section className="main__container">
       <Section className="main__wrapper">
@@ -49,7 +104,7 @@ const Lunch = () => {
             <Paragraf className="lunch__title" text="Lunch"></Paragraf>
             <Div className="lunch__div">
               <Div className="lunch__firstItem">
-                <select>
+                <select onChange={handleSelectChange} value={select}>
                   <option>Choose items</option>
                   {data.map((item) => {
                     return (
@@ -59,8 +114,8 @@ const Lunch = () => {
                     );
                   })}
                 </select>
-                <Input></Input>
-                <Button></Button>
+                <Input value={grams} onChange={onChangeGrams}></Input>
+                <Button onClick={AddItemButton}></Button>
               </Div>
               <LunchExistingItems items={items} />
             </Div>
