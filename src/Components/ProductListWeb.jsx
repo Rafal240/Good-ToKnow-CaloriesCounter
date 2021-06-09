fa35e61200ee54e24e4368ed216fbb9c64c8f9a7
Rafal240 <73700997+Rafal240@users.noreply.github.com>
2021-06-09 20:31:26 +0200

    bordery i buttony

--- src/Components/ProductListWeb.jsx
+++ src/Components/ProductListWeb.jsx
@@ -31,46 +31,71 @@ const ProductListWeb = () => {
         <Div className="savedList__wrapper">
           <Div className="savedList__form">
             <Div className="savedList__items">
-              <div className="savedList__item">
-                <Div>Product Id</Div>
-                <Div>Name</Div>
-                <Div>Cal</Div>
-                <Div>Fat</Div>
-                <Div>Protein</Div>
-                <Div>Sugar</Div>
-              </div>
-              {data.map((eachProduct) => {
-                return (
-                  <div key={eachProduct.id} className="savedList__item">
-                    <div>{eachProduct.id}</div>
-                    <div>{eachProduct.name}</div>
-                    <div>
-                      <Paragraf
-                        className="savedList__cal"
-                        text={eachProduct.cal}
-                      />
-                    </div>
-                    <div>
-                      <Paragraf
-                        className="savedList__fatl"
-                        text={eachProduct.fat}
-                      />
-                    </div>
-                    <div>
-                      <Paragraf
-                        className="savedList__cal"
-                        text={eachProduct.protein}
-                      />
-                    </div>
-                    <div>
-                      <Paragraf
-                        className="savedList__cal"
-                        text={eachProduct.sugar}
-                      />
-                    </div>
-                  </div>
-                );
-              })}
+              <Div className="savedList__item">
+                <Div className="sv__titles">
+                  <Paragraf className="sv_id" text="Product Id"></Paragraf>
+                </Div>
+                <Div className="sv__titles">
+                  <Paragraf className="sv_name" text="Name"></Paragraf>
+                </Div>
+                <Div className="sv__titles">
+                  <Paragraf className="sv_cal" text="Calories"></Paragraf>
+                </Div>
+                <Div className="sv__titles">
+                  <Paragraf className="sv_fat" text="Fat"></Paragraf>
+                </Div>
+                <Div className="sv__titles">
+                  <Paragraf className="sv_protein" text="Protein"></Paragraf>
+                </Div>
+                <Div className="sv__titles">
+                  <Paragraf className="sv_sugar" text="Sugar"></Paragraf>
+                </Div>
+              </Div>
+              <Div className="productList__mapa">
+                {data.map((eachProduct) => {
+                  return (
+                    <Div key={eachProduct.id} className="savedList__item_map">
+                      <Div className="map__id_tem">
+                        <Paragraf
+                          className="savedList_id"
+                          text={eachProduct.id}
+                        ></Paragraf>
+                      </Div>
+                      <Div className="map__name_item">
+                        <Paragraf
+                          className="savedList_name"
+                          text={eachProduct.name}
+                        ></Paragraf>
+                      </Div>
+
+                      <Div className="map___cal_item">
+                        <Paragraf
+                          className="savedList__cal"
+                          text={eachProduct.cal}
+                        />
+                      </Div>
+                      <Div className="map__fat_item">
+                        <Paragraf
+                          className="savedList__fat"
+                          text={eachProduct.fat}
+                        />
+                      </Div>
+                      <Div className="map__protein_item">
+                        <Paragraf
+                          className="savedList__protein"
+                          text={eachProduct.protein}
+                        />
+                      </Div>
+                      <Div className="map__sugar_item">
+                        <Paragraf
+                          className="savedList__sugar"
+                          text={eachProduct.sugar}
+                        />
+                      </Div>
+                    </Div>
+                  );
+                })}
+              </Div>
             </Div>
           </Div>
         </Div>
